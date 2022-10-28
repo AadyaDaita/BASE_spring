0eba763d0d5c0fd8db8dfa3b4bfe0cb5a71db3ad
briag <briagee101@gmail.com>
2022-10-28 09:05:05 -0700

    new wishlist api

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/wishlist/WishlistInit.java
@@ -0,0 +1,13 @@
+package com.nighthawk.spring_portfolio.mvc.jokes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component // Scans Application for ModelInit Bean, this detects CommandLineRunner
+public class WishlistInit {
+    
+}
